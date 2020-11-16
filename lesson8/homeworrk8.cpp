5a410e80c64d7672860b42cfd4dad8e998d5c85a
SahakN <svmusic1502@gmail.com>
2020-11-16 13:26:50 +0400

    HomeWork8

---
+++ lesson8/homeworrk8.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define SIZE 5
+
+int reverseEdArr[SIZE]{};
+int posArr[SIZE]{};
+int multArr[SIZE]{};
+
+void printArr(int arr[], int size){
+    for(int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int * reverse(int arr[], int size){
+    
+    for(int i = 0; i < size; i++)
+        reverseEdArr[i] = arr[size - 1 - i];
+    return reverseEdArr;
+}
+
+int * changePos(int arr[], int size){
+    
+    for(int i = 0; i < size; i++)
+        (i == 0) ? posArr[0] = arr[size - 1] : (i == size - 1) ? posArr[size - 1] = arr[size - 2] : posArr[i] = arr[i - 1];
+    //for(int i = 0; i < size; i++) cout << posArr[i]; 
+    return posArr;  
+}
+
+int * arrMult(int arr[], int size){
+    for(int i = 0; i < size; i++)
+        (i == size - 1) ? multArr[size - 1] = arr[size - 1] * arr[0] : multArr[i] = arr[i] * arr[i + 1];
+    //for(int i = 0; i < size; i++) cout << multArr[i];
+    return multArr;
+}
+
+
+int multFuncMult(int arr[], int arr1[], int size){
+    int sum = 0;
+    for(int i = 0; i < size; i++)
+        sum += arr[i] * arr1[i];
+    return sum;
+}
+
+int main(){
+    int inputArr[SIZE]{};
+    for(int i = 0; i < SIZE; i++)
+        cin >> inputArr[i];
+    int* addr1;
+    int* addr2;
+    int* addr3;
+    addr1 = reverse(inputArr, SIZE);
+    printArr(addr1, SIZE);
+    addr2 = changePos(inputArr, SIZE);
+    addr1 = reverse(addr2, SIZE);
+    printArr(addr1, SIZE);
+    addr3 = arrMult(inputArr, SIZE);
+    addr1 = reverse(addr3, SIZE);
+    printArr(addr1, SIZE);
+    addr2 = changePos(inputArr, SIZE);
+    addr3 = arrMult(inputArr, SIZE);
+    cout << multFuncMult(addr2, addr3, SIZE);
+}
